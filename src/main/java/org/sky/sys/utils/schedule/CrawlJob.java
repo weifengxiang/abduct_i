4de2df2447fd744ad17215e912ac3fb6c1074a52
weifengxiang <442695429@qq.com>
2019-05-07 15:43:43 +0800

    /报错引起中断

--- src/main/java/org/sky/sys/utils/schedule/CrawlJob.java
+++ src/main/java/org/sky/sys/utils/schedule/CrawlJob.java
@@ -1,5 +1,6 @@
 package org.sky.sys.utils.schedule;
 
+import org.apache.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -7,7 +8,7 @@ import org.sky.sjzq.service.TbStSjzqService;
 import org.sky.sys.utils.BspUtils;
 
 public class CrawlJob  implements Job {
-
+	private final Logger logger=Logger.getLogger(CrawlJob.class);
 	@Override
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
 		// TODO Auto-generated method stub
@@ -19,7 +20,13 @@ public class CrawlJob  implements Job {
 			System.out.println(i);
 			String xqbhStr=i+"";
 			if(sjzqService.countByXqbh(xqbhStr)==0) {
-				sjzqService.crawlData(xqbhStr);
+				logger.info("抓取"+xqbhStr+"... ...");
+				try {
+					sjzqService.crawlData(xqbhStr);
+				}catch(Exception e) {
+					e.printStackTrace();
+					logger.error("抓取"+xqbhStr+"出错:"+e.getMessage());
+				}
 			}
 		}
 		
